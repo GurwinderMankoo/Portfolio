d74cfddea0e05fe219359a282fae97698ce8bf6f
GurwinderMankoo <gurwinder.cu@gmail.com>
2023-10-20 23:07:41 +0100

    uff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,18 @@ import AboutMain from './components/about/AboutMain'
 import Technologies from './components/techSection/Technologies'
 import ProjectsMain from './components/projects/ProjectsMain'
 import UpArrow from './components/common/UpArrow'
-import Main from './components/common/Main'
 
 export default function Home() {
   return (
     <>
-      {/* <main className="light:bg-gradient-to-b light:from-[#D6DBDC] light:to-white text-black dark:bg-gray-900/90 dark:text-white"> */}
-      <Main>
+      <main className="bg-white text-black dark:bg-gray-900/90 dark:text-white">
         <HomeMain1 />
         <AboutMain />
         <Experience />
         <Technologies />
         <ProjectsMain />
         <ContactMain />
-      </Main>
-      {/* </main> */}
+      </main>
       <UpArrow />
     </>
   )
