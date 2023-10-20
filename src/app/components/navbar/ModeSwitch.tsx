1c16187da11fe8002959e02e2386b52ed2591539
GurwinderMankoo <gurwinder.cu@gmail.com>
2023-10-20 21:56:52 +0100

    Added Theme switching library

--- src/app/components/navbar/ModeSwitch.tsx
+++ src/app/components/navbar/ModeSwitch.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { useModeSwitcher } from '@/app/store/ModeSwitcherProvider'
+// import { useModeSwitcher } from '@/app/store/ModeSwitcherProvider'
 import React from 'react'
+import { useTheme } from 'next-themes'
 
 type Props = {
     className: string
@@ -8,17 +9,19 @@ type Props = {
 
 export default function ModeSwitch({ className }: Props) {
 
-    const { mode, setMode } = useModeSwitcher()
+    // const { mode, setMode } = useModeSwitcher()
+    const { theme, setTheme } = useTheme()
+
 
     return (
         <li className='flex items-center'>
             <a
                 role='button'
                 className={`block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ${className}`}
-                onClick={() => setMode((prevState) => prevState === "light" ? 'dark' : 'light')}
+                onClick={() => setTheme(theme === "light" ? 'dark' : 'light')}
             >
                 {
-                    mode === "dark" ?
+                    theme === "dark" ?
                         <svg data-toggle-icon="sun" className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M10 15a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0-11a1 1 0 0 0 1-1V1a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1Zm0 12a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1ZM4.343 5.757a1 1 0 0 0 1.414-1.414L4.343 2.929a1 1 0 0 0-1.414 1.414l1.414 1.414Zm11.314 8.486a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM4 10a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h2a1 1 0 0 0 1-1Zm15-1h-2a1 1 0 1 0 0 2h2a1 1 0 0 0 0-2ZM4.343 14.243l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414a1 1 0 0 0-1.414-1.414ZM14.95 6.05a1 1 0 0 0 .707-.293l1.414-1.414a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 .707 1.707Z"></path>
                         </svg>
--- src/app/components/navbar/NavLinks.tsx
+++ src/app/components/navbar/NavLinks.tsx
@@ -63,16 +63,6 @@ export default function NavLinks({ }: Props) {
                             </li>
                         ))
                     }
-
-                    {/* <li>
-                        <a href="#" onClick={() => pathHandler('/')} className={`block py-2 pl-3 pr-4 ${'/' === currentPath ? 'bg-blue-700' : 'text-white'} rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500`} aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <a href="#about" onClick={() => pathHandler('#about')} className={`block py-2 pl-3 pr-4  ${'#about' === currentPath ? 'bg-blue-700' : 'text-gray-900'} rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>About</a>
-                    </li>
-                    <li>
-                        <a href="#contacts" onClick={() => pathHandler('#contacts')} className={`block py-2 pl-3 pr-4 ${'#contacts' === currentPath ? 'bg-blue-700' : 'text-gray-900'} rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Contact</a>
-                    </li> */}
                     <ModeSwitch className="md:block hidden" />
                 </ul>
             </div>
