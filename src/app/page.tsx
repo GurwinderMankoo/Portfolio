498b0c0dfcb93c31211c6c40a16a83088039e4f2
GurwinderMankoo <gurwinder.cu@gmail.com>
2023-11-02 16:11:09 +0000

    Home file name changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import ContactMain from './components/contact/ContactMain'
 import Experience from './components/Experience/page'
-import HomeMain1 from './components/home/HomeMain1'
+import HomeMain from './components/home/HomeMain'
 import AboutMain from './components/about/AboutMain'
 import Technologies from './components/techSection/Technologies'
 import ProjectsMain from './components/projects/ProjectsMain'
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <main className="bg-new-gradient text-black dark:bg-gray-900/90 dark:text-white">
-        <HomeMain1 />
+        <HomeMain />
         <AboutMain />
         <Experience />
         <Technologies />
